5dd6afa1dc7457a9857693e513fbb341c23b5ac0
Ondrej Bercik <ondrej.bercik@thermofisher.com>
2025-11-17 11:32:38 +0100

    Initial commit - setting router with tailwind CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './pages/home/home-page';
+import { UserPage } from './pages/users/user-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: HomePage
+  },
+  {
+    path: 'users',
+    title: 'User Page',
+    component: UserPage
+  }
+];
